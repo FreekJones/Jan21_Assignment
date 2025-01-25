3d7b40c4a222b820d9fb634a1cb98ce6050697bf
FreekJones <noah.d.orourke@gmail.com>
2025-01-25 13:23:42 -0500

    New files for templates

--- src/main.py
+++ src/main.py
@@ -1,38 +1,52 @@
 import cherrypy
-import os.path
+import os
+from mako.lookup import TemplateLookup
 
-# Importing the pages
-import page_index
-import page_signup
-import page_posts
+# Configure Mako template lookup
+srcdir = os.path.abspath(os.path.dirname(__file__))
+lookup = TemplateLookup(directories=[os.path.join(srcdir, "../html")])
 
-class App:            #Main class
-    @cherrypy.expose  #Home page
+class App:
+    @cherrypy.expose
     def index(self):
-        return page_index.get()
-    
+        template = lookup.get_template("index.html")
+        return template.render(title="Home Page", name="NAME")
+
     @cherrypy.expose
     def signup(self):
-        return page_signup.get()
-    
+        template = lookup.get_template("signup.html")
+        return template.render(title="Signup", name="NAME")
+
     @cherrypy.expose
     def posts(self):
-        return page_posts.get()
+        users = [
+            "Maksymilian Hewitt", "Chad Green", "Zakaria Glenn", "Salman Erickson",
+            "Khadija Rosario", "Jemima Humphrey", "Melvin Kirby", "Lowri Henry",
+            "Shawn Lowery", "Gertrude Bradford"
+        ]
+        posts = [
+            {
+                "user": user,
+                "thumbnail": f"/html/images/avatar{i + 1}.png",
+                "time_ago": f"{random.randint(1, 30)} days ago",
+                "views": random.randint(0, 1000),
+            }
+            for i, user in enumerate(users)
+        ]
+        template = lookup.get_template("posts.html")
+        return template.render(title="Posts Page", posts=posts, name="NAME")
 
-srcdir = os.path.abspath(os.path.dirname(__file__))
-
-app = App()
-cherrypy.quickstart(
-    app,
-    '/',
-    {
-        "/html": 
+if __name__ == "__main__":
+    cherrypy.quickstart(
+        App(),
+        "/",
         {
-            "tools.staticdir.on": True,
-            "tools.staticdir.dir": f"{srcdir}/../html"
-        }
-    }
-)
+            "/html": {
+                "tools.staticdir.on": True,
+                "tools.staticdir.dir": os.path.join(srcdir, "../html"),
+            }
+        },
+    )
 
 #I used the following resources to help me with this project:
 #https://www.w3schools.com/html/
